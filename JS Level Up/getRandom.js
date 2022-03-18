12a1bd227e31bef88be818a5a9026814ee0fc200
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-03-19 02:02:57 +0900

    Named Export

---
+++ JS Level Up/getRandom.js
@@ -0,0 +1,10 @@
+export function random() {
+    return Math.floor(Math.random() * 10)
+}
+export const user = {
+    name : 'uchan',
+    age : 28
+}
+
+// named export와 default export 동시 사용이 가능.
+export default 123
