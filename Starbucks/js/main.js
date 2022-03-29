af1bde4ee4bee99e872f2641031e2e3366df7dcf
Yuchan <75283773+yuchan509@users.noreply.github.com>
2022-03-29 10:38:31 +0900

    Js function

--- Starbucks/js/main.js
+++ Starbucks/js/main.js
@@ -71,3 +71,68 @@ new Swiper('.notice-line .swiper-container', {
     autoplay: true,
     loop: true
 })
+
+new Swiper('.promotion .swiper-container', {
+    // direction: 'horizontal' : defalut 값.
+    // 한 번에 보여줄 슬라이드 개수
+    slidesPerView : 3, 
+    // 슬라이드 사이 여백
+    spaceBetween: 10, 
+    // 1번 슬라이드가 가운데 보이기
+    centeredSlides: true,
+    // 반복 재생 여부
+    loop: true,
+    autoplay: { delay: 5000 },
+    // 페이지 번호 사용 여부
+    pagination: { 
+        // 페이지 번호 요소 선택자
+        el: '.promotion .swiper-pagination',
+        clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+    },
+    // 슬라이드 이전/다음 버튼 사용 여부.
+    navigation: { 
+        // 이전 버튼 선택자.
+        prevEl: '.promotion .swiper-prev',
+        // 다음 버튼 선택자 .
+        nextEl: '.promotion .swiper-next' 
+      }
+})
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromtion = false
+promotionToggleBtn.addEventListener('click', function () {
+    // !: 반대 값이 되도록
+    isHidePromtion = !isHidePromtion
+    if (isHidePromtion) {
+        // 숨김 처리.
+        promotionEl.classList.add('hide')
+
+    } else {
+        // 보임 처리.
+        promotionEl.classList.add('remove')
+    }
+})
+
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 '문자 데이터'를,
+    // `parseFloat()`을 통해 소수점을 가지는 '숫자 데이터'로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  }
+
+function floatingObject(selector, delay, size) {
+gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+    delay: random(0, delay), // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
+    y: size, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
+    repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
+    yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
+    ease: Power1.easeInOut // Easing 함수 적용.
+    }
+)
+}
+floatingObject('.floating1', 1, 15)
+floatingObject('.floating2', .5, 15)
+floatingObject('.floating3', 1.5, 20)
